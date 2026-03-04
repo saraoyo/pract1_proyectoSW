ec57fa47d701e2e937f0c409c6f88f14154ffdef
SaraEcheverri <sara_echeverri82232@elpoli.edu.co>
2026-03-04 17:26:30 -0500

    agrega un comentario

--- PagoService.java
+++ PagoService.java
@@ -7,10 +7,11 @@ public class PagoService {
                 throw new Exception("Monto inválido");
             } else {
                 System.out.println("Procesando pago de " + monto);
-
+            //se deberia restar el monto a la cuenta
             }
         } catch (Exception e) {
             System.out.println("Error");
         }
     }
+
 }
